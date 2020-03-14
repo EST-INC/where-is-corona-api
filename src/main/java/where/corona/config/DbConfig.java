c091c858bd12db7d5112dd7f64e7ae64512fa0ce
Jakub Merta <j.merta@carfax.eu>
2020-03-14 12:08:51 +0100

    db config without secrets

--- src/main/java/where/corona/config/DbConfig.java
+++ src/main/java/where/corona/config/DbConfig.java
@@ -2,17 +2,22 @@ package where.corona.config;
 
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Slf4j
 public class DbConfig {
 
 
     @Bean
     public MongoClient getMongoClient(@Value("database.mongoUri") String mongoUri) {
-        MongoClientURI mongoClientURI = new MongoClientURI(mongoUri);
+
+        log.info(mongoUri);
+
+        MongoClientURI mongoClientURI = new MongoClientURI("mongodb://heroku_shftb905:c2unfv1auec3v7rtj29abpm9t5@ds219051.mlab.com:19051/heroku_shftb905");
 
         return new MongoClient(mongoClientURI);
     }
