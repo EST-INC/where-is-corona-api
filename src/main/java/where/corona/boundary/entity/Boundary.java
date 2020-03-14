a496eb71ab238a2ebee84d879c99f01390154b07
Jakub Merta <j.merta@carfax.eu>
2020-03-14 13:48:54 +0100

    boundaries

--- src/main/java/where/corona/boundary/entity/Boundary.java
+++ src/main/java/where/corona/boundary/entity/Boundary.java
@@ -1,16 +1,24 @@
-package where.corona.Model;
+package where.corona.boundary.entity;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Properties;
 
 @Data
+@Builder
+@AllArgsConstructor
+@Document
 public class Boundary {
 
     @JsonProperty("type")
     private String type;
     @JsonProperty("id")
+    @Id
     private Integer id;
     @JsonProperty("properties")
     private Properties properties;
--- src/main/java/where/corona/boundary/entity/BoundaryProperties.java
+++ src/main/java/where/corona/boundary/entity/BoundaryProperties.java
@@ -1,9 +1,14 @@
-package where.corona.Model;
+package where.corona.boundary.entity;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
+
 @Data
+@Builder
+@AllArgsConstructor
 public class BoundaryProperties {
     @JsonProperty("id")
     private Integer id;
