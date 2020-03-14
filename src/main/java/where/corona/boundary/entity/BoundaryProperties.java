f44d2ed94dd799ff29edeb9dd61abc8d26b0d9b9
Jakub Merta <j.merta@carfax.eu>
2020-03-14 14:22:26 +0100

    Save boundaries

--- src/main/java/where/corona/boundary/entity/BoundaryProperties.java
+++ src/main/java/where/corona/boundary/entity/BoundaryProperties.java
@@ -1,17 +1,39 @@
 package where.corona.boundary.entity;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 
-@Data
-@Builder
-@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "id",
+        "nazwa"
+})
 public class BoundaryProperties {
+
     @JsonProperty("id")
     private Integer id;
     @JsonProperty("nazwa")
     private String nazwa;
+
+    @JsonProperty("id")
+    public Integer getId() {
+        return id;
+    }
+
+    @JsonProperty("id")
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @JsonProperty("nazwa")
+    public String getNazwa() {
+        return nazwa;
+    }
+
+    @JsonProperty("nazwa")
+    public void setNazwa(String nazwa) {
+        this.nazwa = nazwa;
+    }
 }
--- src/main/java/where/corona/boundary/service/BoundaryService.java
+++ src/main/java/where/corona/boundary/service/BoundaryService.java
@@ -1,19 +1,50 @@
 package where.corona.boundary.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 import where.corona.boundary.entity.Boundary;
+import where.corona.boundary.entity.BoundaryCollection;
 import where.corona.boundary.repository.BoundaryRepository;
 
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class BoundaryService {
 
     private final BoundaryRepository boundaryRepository;
+    private final ObjectMapper mapper;
 
     public List<Boundary> getAllBoundaries() {
         return boundaryRepository.findAll();
     }
+
+
+    public void prepareData(List<Boundary> boundaries) {
+        boundaryRepository.deleteAll();
+
+        boundaryRepository.saveAll(boundaries);
+    }
+
+
+    @PostConstruct
+    private void init() throws IOException {
+        InputStream inputStream = new ClassPathResource("poland-boundaries.json").getInputStream();
+
+        try {
+            BoundaryCollection boundaries = mapper.readValue(inputStream, BoundaryCollection.class);
+            prepareData(boundaries.getFeatures());
+            log.info("Saved boundaries");
+        } catch (IOException e) {
+            log.error("Unable to save boundaries: " + e.getMessage());
+        }
+    }
 }
+
