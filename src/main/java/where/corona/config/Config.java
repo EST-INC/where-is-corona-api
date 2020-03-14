4aed1d9553f6325a6e2f27a172d6f8e5a6dcb431
Jakub Merta <j.merta@carfax.eu>
2020-03-14 14:44:44 +0100

    Disabled cors filter

--- src/main/java/where/corona/config/Config.java
+++ src/main/java/where/corona/config/Config.java
@@ -4,6 +4,9 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class Config {
@@ -12,5 +15,18 @@ public class Config {
     public ObjectMapper getMapper() {
         return new ObjectMapper()
                 .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true); // you USUALLY want this
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("GET");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
